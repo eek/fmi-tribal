cd0c2b1eedf3ea634429d0663e39651c5ff9c177
Radu-Sebastian Amarie <radu@findie.me>
2017-05-16 18:18:48 +0300

    ADD: Assets ADD: Initial Create-React-App ADD: Initial Router

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainRouter from './MainRouter';
+import './index.css';
+
+ReactDOM.render(
+  <MainRouter />,
+  document.getElementById('root')
+);
